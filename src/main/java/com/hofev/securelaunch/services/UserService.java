66c7b50a30cebd83f1e36ee79db295c4f17580f3
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-11-24 16:53:38 +0300

    # 0.4.9 Реализация метода UserService.registrationUser

--- src/main/java/com/hofev/securelaunch/services/UserService.java
+++ src/main/java/com/hofev/securelaunch/services/UserService.java
@@ -1,19 +1,49 @@
 package com.hofev.securelaunch.services;
 
 import com.hofev.securelaunch.exceptions.InvalidPasswordException;
+import com.hofev.securelaunch.exceptions.UserAlreadyExistException;
 import com.hofev.securelaunch.exceptions.UserNotFoundException;
 import com.hofev.securelaunch.models.User;
 import com.hofev.securelaunch.repositories.UserRepository;
 import com.hofev.securelaunch.utils.HashingUtil;
 
 public class UserService {
+    private final UserRepository userRepository;
+
+    UserService() {
+        this.userRepository = new UserRepository();
+    }
 
 
     // Вход пользователя
-    public static void loginUser(String login, String password) throws UserNotFoundException, InvalidPasswordException {
-        UserRepository userRepository = new UserRepository();
+    public void loginUser(String login, String password) throws UserNotFoundException, InvalidPasswordException {
         User user = userRepository.findUserByLogin(login);
 
         if (!(user.getPassword().equals(HashingUtil.hash256(password)))) throw new InvalidPasswordException("Не подходит пароль!");
+    }
+
+    // Регистрация пользователя
+    public void registrationUser(String[] dataUser) throws UserAlreadyExistException {
+//        String login;
+//        String name;
+//        String surname;
+//        String phone;
+//        String email;
+//        String password;
+
+        try {
+            userRepository.findUserByLogin(dataUser[0]);
+            throw new UserAlreadyExistException("Пользователь с таким логином существует!");
+        } catch (UserNotFoundException e) {
+            userRepository.saveUser(new User(
+                    dataUser[0],
+                    dataUser[1],
+                    dataUser[2],
+                    dataUser[3],
+                    dataUser[4],
+                    dataUser[5]
+            ));
+        }
+
     }
 }
