8525376c94360d6c503e97b6fb0cfc32bda93f5d
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-05 08:16:48 +0300

    # 1.0.3 Реализация LoginBlockedException

---
+++ src/main/java/com/hofev/securelaunch/exceptions/LoginBlockedException.java
@@ -0,0 +1,7 @@
+package com.hofev.securelaunch.exceptions;
+
+public class LoginBlockedException extends Exception{
+    public LoginBlockedException(String message) {
+        super(message);
+    }
+}
