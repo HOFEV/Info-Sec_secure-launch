3a03502d8da22e961b5517a4b822c3ac98d989c5
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-14 23:12:18 +0300

    # 1.1.4 Добавление обработки ошибки, в случае присваивания несуществующего уровня доступа

---
+++ src/main/java/com/hofev/securelaunch/exceptions/NonExistAccessLevel.java
@@ -0,0 +1,7 @@
+package com.hofev.securelaunch.exceptions;
+
+public class NonExistAccessLevel extends Exception {
+    public NonExistAccessLevel(String message) {
+        super(message);
+    }
+}
