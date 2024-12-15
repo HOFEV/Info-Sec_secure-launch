254a0858d95ba1d23e81ad6f20bc4f6d0892f9e6
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-15 08:22:29 +0300

    # 1.1.7 Добавление инициализации админа при запуске приложения

---
+++ src/main/java/com/hofev/securelaunch/modules/userAccessLevel/AccessLevelService.java
@@ -0,0 +1,38 @@
+package com.hofev.securelaunch.modules.userAccessLevel;
+
+import com.hofev.securelaunch.exceptions.UserAlreadyExistException;
+import com.hofev.securelaunch.exceptions.UserNotFoundException;
+import com.hofev.securelaunch.models.User;
+import com.hofev.securelaunch.repositories.UserRepository;
+
+public class AccessLevelService {
+    private AccessLevelService() {}
+
+    // Инициализация аккаунта админа
+    public static void initAdminAcc() {
+        UserRepository userRepository = new UserRepository();
+
+        try {
+            userRepository.findUserByLogin(AccessLevel.ADMIN.name());
+        } catch (UserNotFoundException e) {
+            createDefaultAdminAcc();
+        }
+    }
+
+    // Создает начальный аккаунт админа
+    private static void createDefaultAdminAcc() {
+        try {
+            User admin = new User(
+                    "admin",
+                    "Иван",
+                    "Иванов",
+                    "",
+                    "@gmail.com",
+                    "9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0"
+            );
+            admin.setUserLevelAccess(AccessLevel.ADMIN);
+            (new UserRepository()).saveUser(admin);
+        } catch (UserAlreadyExistException _) {
+        }
+    }
+}
