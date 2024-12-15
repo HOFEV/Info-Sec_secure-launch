41d1e29d51cffa92de4bb5b12321946ac22d84af
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-15 08:25:15 +0300

    # 1.1.8 Удаление лишней ошибки

--- src/main/java/com/hofev/securelaunch/exceptions/NonExistAccessLevel.java
+++
@@ -1,7 +0,0 @@
-package com.hofev.securelaunch.exceptions;
-
-public class NonExistAccessLevel extends Exception {
-    public NonExistAccessLevel(String message) {
-        super(message);
-    }
-}
