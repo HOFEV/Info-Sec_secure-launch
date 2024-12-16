d706334d335baf2f374e27725e698b4e1055ac31
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-16 12:15:17 +0300

    # 1.1.23 AccessLevel Добавление свойства изменяемости роли

--- src/main/java/com/hofev/securelaunch/modules/userAccessLevel/AccessLevel.java
+++ src/main/java/com/hofev/securelaunch/modules/userAccessLevel/AccessLevel.java
@@ -3,17 +3,23 @@ package com.hofev.securelaunch.modules.userAccessLevel;
 public enum AccessLevel {
 
     // Права доступа
-    USER(false),
-    EDITOR(true),
-    ADMIN(true);
+    USER(true , false),
+    EDITOR(true, true),
+    ADMIN(false, true);
 
     private final boolean editingRight;
+    private final boolean settingRight;
 
-    AccessLevel(boolean editingRight) {
+    AccessLevel(boolean settingRight ,boolean editingRight) {
+        this.settingRight = settingRight;
         this.editingRight = editingRight;
     }
 
     public boolean isEditingRight() {
         return editingRight;
+    }
+
+    public boolean isSettingRight() {
+        return settingRight;
     }
 }
