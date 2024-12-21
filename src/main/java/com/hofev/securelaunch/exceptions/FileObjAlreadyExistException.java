44a8872f435f6bda6fab4f2a17a16031b221b851
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-21 22:06:38 +0300

    # 1.2.19 добавление исключения FileObjAlreadyExistException

---
+++ src/main/java/com/hofev/securelaunch/exceptions/FileObjAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.hofev.securelaunch.exceptions;
+
+public class FileObjAlreadyExistException extends Exception {
+    public FileObjAlreadyExistException(String message) {
+        super(message);
+    }
+}
