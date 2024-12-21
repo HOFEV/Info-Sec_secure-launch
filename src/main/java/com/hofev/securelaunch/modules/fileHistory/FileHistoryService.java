dc406f75320f7c1e8de2e3bb574393ed7d1f9283
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-21 23:16:52 +0300

    # 1.2.22 Переписаны все методы FileHistoryService

--- src/main/java/com/hofev/securelaunch/modules/fileHistory/FileHistoryService.java
+++ src/main/java/com/hofev/securelaunch/modules/fileHistory/FileHistoryService.java
@@ -1,5 +1,6 @@
 package com.hofev.securelaunch.modules.fileHistory;
 
+import com.hofev.securelaunch.exceptions.FileObjNotFoundException;
 import com.hofev.securelaunch.repositories.FileObjRepository;
 import com.hofev.securelaunch.services.FileService;
 import com.hofev.securelaunch.utils.HashingUtil;
@@ -14,13 +15,29 @@ public class FileHistoryService {
     // Обновление хэш содержимого файла
     public static void updateHashFileContent(File file, String content) {
         // Изменение хэш содержимого файла в истории файлов
-        FileObjRepository.getInstance().getFileObjByName(file.getName()).setHashData(getHashOfContentFile(content));
+        try {
+            FileObjRepository.getInstance().getFileObjByName(file.getName()).setHashData(getHashOfContentFile(content));
+        } catch (FileObjNotFoundException e) {
+            try {
+                FileObjRepository.getInstance().createFileObj(file);
+            } catch (IOException _) {}
+        }
+    }
+
+    public static boolean isFileValid(File file) throws IOException {
+        try {
+            // Проверяется совместимость хэша
+            return checkMatchOfHashFileContent(file);
+        } catch (FileObjNotFoundException e) {
+            return true;
+        }
     }
 
     // Проверка на совпадение хэша содержимого
-    public static boolean checkMatchOfHashFileContent(File file) throws IOException {
+    private static boolean checkMatchOfHashFileContent(File file) throws IOException, FileObjNotFoundException {
         // Получение хэш содержимого файла из базы
-        String hashFromFileData = FileObjRepository.getInstance().getFileObjByName(file.getName()).getHashData();
+        String hashFromFileData = null;
+        hashFromFileData = FileObjRepository.getInstance().getFileObjByName(file.getName()).getHashData();
         // Получение хэш содержимого текущего файла
         String hashFromFile = getHashOfContentFile(FileService.readFileContent(file));
         return hashFromFile.equals(hashFromFileData);
