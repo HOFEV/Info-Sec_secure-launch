e46bfed93ecad3c40811f045d6ef30d3dd5776f0
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-23 15:44:09 +0300

    # 1.2.25 Удаление лишнего исключения

--- src/main/java/com/hofev/securelaunch/exceptions/FileObjNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hofev.securelaunch.exceptions;
-
-public class FileObjNotFoundException extends Exception {
-    public FileObjNotFoundException(String message) {
-        super(message);
-    }
-}
