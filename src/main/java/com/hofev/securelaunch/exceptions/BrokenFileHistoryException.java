a1eb1f072ae3b846845e3e2ea5a300eb3cde4a8f
Ladygin_Dmitry <daily.hofe@gmail.com>
2024-12-23 15:43:19 +0300

    # 1.2.24 Добавление исключения BrokenFileHistoryException

---
+++ src/main/java/com/hofev/securelaunch/exceptions/BrokenFileHistoryException.java
@@ -0,0 +1,7 @@
+package com.hofev.securelaunch.exceptions;
+
+public class BrokenFileHistoryException extends Exception {
+    public BrokenFileHistoryException(String message) {
+        super(message);
+    }
+}
